d9933f484562555b4a11c50c657d1e92556a8b8e
Riccardo Spezia <124380883+rspezia@users.noreply.github.com>
2026-01-29 20:30:48 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+echo "# my_firs_binder" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rspezia/my_firs_binder.git
+git push -u origin main
