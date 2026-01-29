10ae128d3cde3a788186e2c8f18aad11a3292b43
Riccardo Spezia <124380883+rspezia@users.noreply.github.com>
2026-01-29 20:43:48 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,1 @@
 print("Hello from Binder!") 
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/rspezia/my_firs_binder.git
-git push -u origin main
